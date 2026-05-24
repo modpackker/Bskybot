81f57e8e36a6e7191f268eb87f4ff86ecbc5990b
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-05-24 11:53:08 +0200

    chore: uninstall dotenv

--- src/data/bluesky.ts
+++ src/data/bluesky.ts
@@ -1,10 +1,6 @@
 import { env } from 'node:process';
 
-import dotenv from 'dotenv';
-
 import { BlueskyClient } from '../lib/bluesky.js';
-
-dotenv.config();
 
 export const bskyClient = new BlueskyClient({
 	identifier: env['BLUESKY_IDENTIFIER']!,
